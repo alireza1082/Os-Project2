c956eca793feb2696547a69b8879375f0e00d232
alireza1082 <rahmani.alireza14@gmail.com>
2023-07-10 07:53:07 +0000

    complete code1.c

--- ucontext/code1.c
+++ ucontext/code1.c
@@ -4,14 +4,16 @@
 #include <unistd.h>
 
 int main() {
-    // int done = 0;
+   int done = 0;
     ucontext_t one, two;
     getcontext(&one);
     printf("Running...\n");
+    printf("%d\n", done);
     sleep(1);
-    // if (!done) {
-    //  done = 1;
+    if (!done) {
+    done = 1;
+    printf("%d\n", done);
     swapcontext(&two, &one);
-    //}
+    }
     return 0;
 }
