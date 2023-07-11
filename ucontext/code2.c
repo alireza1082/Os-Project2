e41ebc4a0c3e4f64769ca84e0f71cd1aa1180a6d
alireza <rezaali1387@gmail.com>
2023-07-11 13:32:10 +0330

    add done context to code2.c

--- ucontext/code2.c
+++ ucontext/code2.c
@@ -6,18 +6,23 @@ static int running_process_id;
 static ucontext_t mContext[2];
 static ucontext_t mContext_main;
 static ucontext_t mContext_done;
-static int p1_flag;
+static int p1_flag1;
+static int p1_flag2;
+static int p2_flag1;
+static int p2_flag2;
 
 void yield()
 {
     printf("yield from pid %d to pid %d\n", running_process_id, 1 - running_process_id);
     if (running_process_id == 0)
     {
+        p1_flag1 = 1;
         running_process_id = 1;
         swapcontext(&mContext[0], &mContext[1]);
     }
     else
     {
+        p2_flag1 = 1;
         running_process_id = 0;
         swapcontext(&mContext[1], &mContext[0]);
     }
@@ -30,6 +35,26 @@ void some_job(int pid, int max_num)
     yield();
     for(int i = 1; i <= max_num; i++)
         printf("pid %d -> %d\n", pid, i);
+    if (pid == 0)
+        p1_flag2 = 1;
+    else if (pid == 1)
+        p2_flag2 = 1;
+    go_to_done(pid)
+}
+
+void go_to_done(int pid){
+    printf("switch to done context")
+    swapcontext(&mContext[pid], mContext_done);
+}
+
+void make_done(int pid){
+    printf("make done context")
+    makecontext(&mContext[0], (void(*) (void)) switch_to_main, 0);
+}
+
+void switch_to_main(){
+    printf("switch to main context from done context")
+    swapcontext(&mContext_done, &mContext_main);
 }
 
 int main(int argc, char *argv[])
@@ -40,11 +65,15 @@ int main(int argc, char *argv[])
     getcontext(&mContext[0]);
     mContext[0].uc_stack.ss_sp = stack1;
     mContext[0].uc_stack.ss_size = sizeof(stack1);
+    p1_flag1 = 0;
+    p1_flag2 = 0;
     makecontext(&mContext[0], (void(*) (void)) some_job, 2, 1, 3);
 //-----------------------------------------------------------------
     getcontext(&mContext[1]);
     mContext[1].uc_stack.ss_sp = stack2;
     mContext[1].uc_stack.ss_size = sizeof(stack2);
+    p2_flag1 = 0;
+    p1_flag2 = 0;
     makecontext(&mContext[1], (void(*) (void))some_job, 2, 2, 5);
     
     running_process_id = 0;
